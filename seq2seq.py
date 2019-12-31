d1fb70097d9019c4be00630294e87bccb5a0eb07
lihao <imagelihao@163.com>
2019-12-31 18:06:01 +0800

    format seq2seq

--- seq2seq.py
+++ seq2seq.py
@@ -8,7 +8,8 @@ Author : LH
 Date : 2019/12/27
 """
 import tensorflow as tf
-from tensorflow.contrib import seq2seq
+from tensorflow.contrib import seq2seq as s2s
+
 
 class Config(object):
     batch_size = 32
@@ -18,6 +19,23 @@ class Config(object):
     output_sequence_size = 20
     vocab_size = 50000
 
+
 class SimpleSeq2Seq(object):
-    def __init__(self,config):
+    def __init__(self, config):
         ...
+
+    def train(self):
+        # 构造输入
+        # 编写encoder
+        # 编写decoder
+        # 输入数据 开始训练
+        ...
+
+    def inference(self):
+        ...
+
+
+tf.enable_eager_execution()
+a = tf.constant(1, shape=[1])
+p = tf.tile(a, [15])
+print(p)
